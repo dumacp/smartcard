6c17013f8472777f2c2ee0cdfb7dc0ce3f47bd7e
Camilo Zapata <ccamilozt@gmail.com>
2018-09-22 07:36:01 -0500

    add dir commands examples

---
+++ nxp/mifare/cmd/main_acl.go
@@ -0,0 +1,156 @@
+package main
+
+
+import (
+        _ "fmt"
+	"flag"
+	"log"
+	"encoding/hex"
+	"strings"
+	"github.com/dumacp/smartcard"
+	"github.com/dumacp/smartcard/nxp/mifare"
+)
+
+var keyS string
+
+func init() {
+	flag.StringVar(&keyS, "key", "00000000000000000000000000000000", "key aes128")
+}
+
+/**/
+func main() {
+	log.Println("Start Logs")
+	flag.Parse()
+	ctx, err := smartcard.NewContext()
+	if err != nil {
+                log.Fatal("Not connection")
+        }
+	defer ctx.Release()
+
+	readers, err := smartcard.ListReaders(ctx)
+	for i, el := range readers {
+		log.Printf("reader %v: %s\n", i, el)
+	}
+
+	var sam mifare.SamAv2
+	var mplus mifare.MifarePlus
+	samReaders := make([]smartcard.Reader,0)
+	for _, el := range readers {
+		if strings.Contains(el, "SAM") {
+			samReaders = append(samReaders, smartcard.NewReader(ctx, el))
+		}
+	}
+
+	for _, samReader := range samReaders {
+		log.Printf("sam reader: %s\n", samReader)
+		//sam, err := samReader.ConnectSamAv2()
+		sam, err = mifare.ConnectSamAv2(samReader)
+		if err != nil {
+			log.Println("%s\n",err)
+		}
+		version, err := sam.GetVersion()
+		if err != nil {
+			log.Println("Not GetVersion: ", err)
+		}
+		log.Printf("GetVersion sam: % X\n", version)
+
+		key, err := hex.DecodeString(keyS)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		resp, err := sam.AuthHostAV2(key, 100)
+		if err != nil {
+			log.Println("Not Auth: ", err)
+		}
+		log.Printf("auth sam: [% X]\n", resp)
+
+		//sam.DisconnectCard()
+	}
+
+	mplusReaders := make([]smartcard.Reader,0)
+	for _, el := range readers {
+		if strings.Contains(el, "PICC") {
+			mplusReaders = append(mplusReaders, smartcard.NewReader(ctx, el))
+		}
+	}
+
+	for _, mplusReader := range mplusReaders {
+		rCounter := 0
+		wCounter := 0
+		log.Printf("mplus reader: %s\n", mplusReader)
+		mplus, err = mifare.ConnectMplus(mplusReader)
+		if err != nil {
+			log.Printf("%s\n",err)
+		}
+		resp, err := mplus.UID()
+		log.Printf("mplus uuid: % X\n", resp)
+		if err != nil {
+			log.Println("%s\n",err)
+		}
+		dataDiv := make([]byte,4)
+		dataDiv = append(dataDiv,resp[0:4]...)
+
+		//resp, err = mplus.FirstAuthf1(0x4002)
+		resp, err = mplus.FirstAuthf1(0x4005)
+		if err != nil {
+			log.Fatalf("%s\n",err)
+		}
+		//resp, err = sam.NonXauthMFPf1(true,3,0x07,0x00,resp,nil)
+		resp, err = sam.NonXauthMFPf1(true,3,0x01,0x00,resp,dataDiv)
+		if err != nil {
+			log.Fatalf("%s\n",err)
+		}
+		log.Printf("aid f2: [% X]\n", resp)
+		resp, err = mplus.FirstAuthf2(resp[0:len(resp)-2])
+		if err != nil {
+			log.Printf("%s\n",err)
+		}
+		resp, err = sam.NonXauthMFPf2(resp)
+		if err != nil {
+			log.Printf("%s\n",err)
+		}
+		log.Printf("auth mplus: [% X]\n", resp)
+
+		resp, err = sam.DumpSessionKey()
+		if err != nil {
+			log.Fatalf("%s\n",err)
+		}
+		log.Printf("sessionKey mplus: [% X]\n", resp)
+
+		keyEnc := resp[0:16]
+		keyMac := resp[16:32]
+		log.Printf("key Mac: [% X]\n", keyMac)
+		Ti := resp[32:36]
+		log.Printf("Ti: [% X]\n", Ti)
+		readCounter := resp[36:38]
+		log.Printf("Read Counter: [% X]\n", readCounter)
+
+		//resp, err = mplus.ReadEncMacMac(4,1,rCounter,wCounter,Ti,keyMac,keyEnc)
+		resp, err = mplus.ReadEncMacMac(11,1,rCounter,wCounter,Ti,keyMac,keyEnc)
+		if err != nil {
+			log.Fatalf("%s\n",err)
+		}
+		log.Printf("read 4 resp: [% X]\n", resp)
+
+		rCounter++
+		resp[5] = 0x0F
+		resp[6] = 0x7F
+		resp[7] = 0x07
+		resp[8] = 0x88
+		resp[9] = 0xFF
+
+		/**/
+		err = mplus.WriteEncMacMac(11,resp,rCounter,wCounter,Ti,keyMac,keyEnc)
+		if err != nil {
+			log.Fatalf("%s\n",err)
+		}
+		/**/
+
+		//mplus.DisconnectCard()
+	}
+	sam.DisconnectCard()
+	mplus.DisconnectCard()
+}
+/**/
+
---
+++ nxp/mifare/cmd/main_perso.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"log"
+	"flag"
+	"strings"
+	"encoding/hex"
+	"github.com/dumacp/smartcard"
+	"github.com/dumacp/smartcard/nxp/mifare"
+)
+
+var keyS string
+var keyNbr int
+
+func init() {
+        flag.StringVar(&keyS, "key", "00000000000000000000000000000000", "key aes128")
+        flag.IntVar(&keyNbr, "keyNbr", 0x4002, "key Number")
+}
+
+func main() {
+	flag.Parse()
+
+	key, err := hex.DecodeString(keyS)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("key: [% X]\n", key)
+
+	ctx, err := smartcard.NewContext()
+	if err != nil {
+		log.Fatal("Not connection")
+	}
+	defer ctx.Release()
+	readers, err := smartcard.ListReaders(ctx)
+	for i, el := range readers {
+		log.Printf("reader %v: %s\n", i, el)
+	}
+	mplusReaders := make([]smartcard.Reader,0)
+	for _, el := range readers {
+		if strings.Contains(el, "PICC") {
+			mplusReaders = append(mplusReaders, smartcard.NewReader(ctx, el))
+		}
+	}
+	for _, mplusReader := range mplusReaders {
+		mplus, err := mifare.ConnectMplus(mplusReader)
+		if err != nil {
+			log.Printf("%s\n",err)
+			continue
+		}
+		uid, err := mplus.UID()
+		if err != nil {
+			log.Fatalln("ERROR: ", err)
+		}
+		log.Printf("card UID: % X\n", uid)
+
+		ats, err := mplus.ATS()
+		if err != nil {
+			log.Println("ERROR: ", err)
+		}
+		log.Printf("card ATS: % X\n", ats)
+
+		/**/
+		resp, err := mplus.WritePerso(keyNbr,key)
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		log.Printf("WritePerso resp: % X\n", resp)
+
+		resp, err = mplus.WritePerso(0x9001,key)
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		log.Printf("WritePerso resp: % X\n", resp)
+
+		/**
+		resp, err = mplus.WritePerso(0x9000,key)
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		log.Printf("WritePerso resp: % X\n", resp)
+
+		resp, err = mplus.WritePerso(0x9002,key)
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		log.Printf("WritePerso resp: % X\n", resp)
+
+		for i:=0; i<16; i++ {
+			keyN := 0x4000 + i
+			resp, err = mplus.WritePerso(keyN,key)
+			if err != nil {
+				log.Fatalf("Error: %s\n",err)
+			}
+			log.Printf("WritePerso resp: % X\n", resp)
+		}
+
+		resp, err = mplus.CommitPerso()
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		log.Printf("Commit Perso resp: % X\n", resp)
+		/**/
+
+		/**
+		resp, err := mplus.TransparentSessionStartOnly()
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		resp, err = mplus.Switch1444_4()
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		resp, err = mplus.FirstAuth(0x9003,key)
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		log.Printf("Auth: resp: % X\n", resp)
+		resp, err = mplus.TransparentSessionEnd()
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		/**
+		resp, err := mplus.FirstAuth(keyNbr,key)
+		if err != nil {
+			log.Fatalf("Error: %s\n",err)
+		}
+		log.Printf("Auth: % X\n", resp)
+		/**/
+		mplus.DisconnectCard()
+	}
+}
+
