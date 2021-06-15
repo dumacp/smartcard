167e8dcd830f7a62f1623f7dc2c7a43ede4f7b83
Camilo Zapata <camilo.zapata@nebulae.com.co>
2021-06-15 15:57:55 -0500

    add EntryKey struct

--- iso7816.go
+++ iso7816.go
@@ -8,3 +8,12 @@ type ISO7816cmd struct {
 	P2  byte
 	Le  bool
 }
+
+func (cmd *ISO7816cmd) PrefixApdu() []byte {
+	apdu := make([]byte, 0)
+	apdu = append(apdu, cmd.CLA)
+	apdu = append(apdu, cmd.INS)
+	apdu = append(apdu, cmd.P1)
+	apdu = append(apdu, cmd.P2)
+	return apdu
+}
